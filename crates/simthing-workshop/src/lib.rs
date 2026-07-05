862d06a41cdebd070b3ecccade2522620ef77aff
khorum08 <khorum08@gmail.com>
2026-07-05 01:49:10 -0500

    TP-FRONTS-AUTHORING-0: author STEAD threat/suppression/disruption fronts

--- crates/simthing-workshop/src/lib.rs
+++ crates/simthing-workshop/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod diplomacy_post_hydration;
+pub mod fronts_post_hydration;
 pub mod eml_phase5;
 pub mod multitarget_replay;
 mod multitarget_replay_report;
@@ -15,6 +16,14 @@ pub mod weighted_mean;
 pub use diplomacy_post_hydration::{
     apply_diplomacy_post_hydration, DiplomacyHydrationError, BASELINE_BORDER_DISTRUST_SURPLUS,
     HOSTILITY_COMMITMENT_EVENT_KIND, HOSTILITY_DISTRUST_THRESHOLD, TP_DISTRUST_RESOURCE_KEY,
+};
+pub use fronts_post_hydration::{
+    apply_fronts_post_hydration, contested_border_settling_oracle, fronts_l3_urgency_col,
+    FrontsHydrationError, TpFrontsAuthoringReport, TpFrontsTheaterCell, DEFAULT_DISRUPTION_INTRINSIC_RATE,
+    DEFAULT_SUPPRESSION_INTRINSIC_RATE, DEFAULT_THREAT_INTRINSIC_RATE, TP_DISRUPTION_ARENA,
+    TP_FRONTS_CHOKE_OUTPUT_COL, TP_FRONTS_DEFAULT_HORIZON, TP_FRONTS_FIELD_OPERATOR_ID,
+    TP_FRONTS_N_DIMS, TP_FRONTS_SOURCE_COL, TP_FRONTS_WEIGHT_PRESSURE, TP_FRONTS_WEIGHT_RESOURCE,
+    TP_SUPPRESSION_ARENA, TP_THREAT_ARENA,
 };
 pub mod weighted_mean_perf;
 mod weighted_mean_perf_report;
